9ef7eb96a810b63ee4cfc88385cfcf0821107f6d
Praveen Kanamarlapudi <kpraveen420@gmail.com>
2015-03-07 21:19:58 -0800

    Two solutions for .. Question: Your task is to help him find the minimum number of characters of the first string he needs to change to enable him to make it an anagram of the second string.

---
+++ HackerRank/src/algorithms/strings/Anagram.java
@@ -0,0 +1,81 @@
+package algorithms.strings;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Anagram {
+
+	//Solution1
+	// public static void main(String[] args) throws IOException {
+	// // Scanner s = new Scanner(System.in);
+	// StringBuffer sb = new StringBuffer();
+	// BufferedReader s = new BufferedReader(new InputStreamReader(System.in));
+	// int testCases = Integer.parseInt(s.readLine());
+	// String line, word[] = new String[2];
+	//
+	//
+	// while (testCases > 0) {
+	// HashMap<Character, Integer> map = new HashMap();
+	// line = s.readLine();
+	// if (line.length() % 2 == 1 || line.length() == 0) {
+	// System.out.println(-1);
+	// testCases--;
+	// continue;
+	// }
+	// word[0] = line.substring(0, line.length() / 2);
+	// word[1] = line.substring(line.length() / 2);
+	// for (int i = 0; i < word[0].length(); i++) {
+	// if (map.get(word[0].charAt(i)) != null)
+	// map.put(word[0].charAt(i), map.get(word[0].charAt(i)) + 1);
+	// else
+	// map.put(word[0].charAt(i), 1);
+	// }
+	//
+	// for (int i = 0; i < word[1].length(); i++) {
+	// if (map.get(word[1].charAt(i)) != null
+	// && map.get(word[1].charAt(i)) > 0)
+	// map.put(word[1].charAt(i),map.get(word[1].charAt(i))-1);
+	// }
+	//
+	// int changeChars = 0;
+	// for (Character c : map.keySet())
+	// changeChars += map.get(c);
+	// System.out.println(changeChars);
+	//
+	// testCases--;
+	// }
+	// }
+
+	//Solution2
+	public static void main(String[] args) throws IOException {
+		StringBuffer sb = new StringBuffer();
+		BufferedReader s = new BufferedReader(new InputStreamReader(System.in));
+		int testCases = Integer.parseInt(s.readLine());
+		String line, word[] = new String[2];
+		while (testCases-- > 0) {
+			short[] charMap = new short[26];
+			short num = 0;
+			line = s.readLine();
+			if (line.length() % 2 == 1 || line.length() == 0) {
+				System.out.println(-1);
+				testCases--;
+				continue;
+			}
+			word[0] = line.substring(0, line.length() / 2);
+			word[1] = line.substring(line.length() / 2);
+			for (int i = 0; i < word[0].length(); i++)
+				charMap[word[0].charAt(i) - 'a'] += 1;
+			for (int i = 0; i < word[1].length(); i++)
+				if (charMap[word[1].charAt(i) - 'a'] > 0)
+					charMap[word[1].charAt(i) - 'a'] -= 1;
+			for (int i = 0; i < 26; num += charMap[i++])
+				;
+			System.out.println(num);
+
+		}
+
+	}
+
+}
